515f90af8d18e52a5dd79c473da7961318dc6343
ben20262 <ben20262@aim.com>
2020-04-15 18:48:28 -0400

    added json to videos

--- backend/app/controllers/videos_controller.rb
+++ backend/app/controllers/videos_controller.rb
@@ -1,2 +1,38 @@
 class VideosController < ApplicationController
+
+    def index
+        videos = Video.all
+        render json: videos
+    end
+
+    def show
+        video = Video.find_by(id: params[:id])
+        if video
+            render json: video
+        else
+            render json: { status: 'failure', message: 'Video not found' }
+        end
+    end
+
+    def create
+        video = Video.create(params[:video])
+
+        if video.valid?
+            render json: video
+        else
+            render json: video.error
+        end
+    end
+
+    def destroy
+        video = Video.find_by(id: params[:id])
+        if video
+            Video.destroy(params[:id])
+            render json: { message: 'Video deleted.' }
+        else
+            render json: { status: 'failure', message: 'Video not found' }
+        end
+        
+    end
+
 end

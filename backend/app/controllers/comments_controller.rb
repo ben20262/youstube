34b5cd4453e78f6ea1fee4b3f780001b413c1161
ben20262 <ben20262@aim.com>
2020-04-15 17:09:20 -0400

    added json

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+    def index
+        comments = Comment.all
+        render json: comments
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment
+    end
+
+    def create
+        comment = Comment.create(params)
+    end
+
 end

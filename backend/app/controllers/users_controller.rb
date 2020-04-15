5e69b6c5f86c3f3a65f68b8072eb673bd8e0c65e
ben20262 <ben20262@aim.com>
2020-04-15 18:48:09 -0400

    added json to users

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.create(username: params[:username])
+
+        if user.valid?
+            render json: user
+        else
+            render json: { message: 'User Invalid' }
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+
+        if user
+            render json: user
+        else
+            render json: { status: 'failure', message: 'User not found' }
+        end
+    end
+
 end

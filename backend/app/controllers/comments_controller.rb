c8ff249665a02f5252bd1a7ffa9bc05169e016c2
ben20262 <ben20262@aim.com>
2020-04-17 16:05:41 -0400

    added user to json

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     def index
         video = Video.find_by(id: params[:video_id])
         if video
-            render json: video.comments
+            render json: video.comments, include: [:user]
         else
             render json: { message: 'Video not found' }
         end
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
         video = Video.find_by(idL params[:video_id])
         comment = Comment.find_by(id: params[:id])
         if comment
-            render json: comment
+            render json: comment, include: [:user]
         else
             render json: { status: 'failure', message: 'Comment not found' }
         end
--- backend/app/controllers/videos_controller.rb
+++ backend/app/controllers/videos_controller.rb
@@ -2,13 +2,13 @@ class VideosController < ApplicationController
 
     def index
         videos = Video.all
-        render json: videos
+        render json: videos, include: :user
     end
 
     def show
         video = Video.find_by(id: params[:id])
         if video
-            render json: video
+            render json: video, include: :user
         else
             render json: { status: 'failure', message: 'Video not found' }
         end
